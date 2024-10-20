c4624baa5a536aa888508461684e012bdfbb0b29
tariqnn <164573713+tariqnn@users.noreply.github.com>
2024-10-20 23:43:46 +0300

    Delete project1 directory

--- project1/src/main/java/org/example/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-// Repository interface
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
